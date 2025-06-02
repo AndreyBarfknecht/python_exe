00efaad89233481e0d09bb64aa6c1135bacaebba
Andrey Barfkencht Rodrigues <andrey30r@gmail.com>
2025-06-02 15:16:50 -0300

    att

--- CODÉDEX/14. Magic 8 Ball.py
+++ CODÉDEX/14. Magic 8 Ball.py
@@ -1,6 +1,6 @@
 """The Magic 8 Ball is a popular office toy and children's toy invented in the 1940s for fortune-telling and advice seeking. 🎱
 
-It's an oversized 8 ball with some of the following answers:
+It's an oversized 8-ball with some of the following answers:
 
     1 Yes - definitely.
     2 It is decidedly so.
