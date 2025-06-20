8986288801d0e996c1f9787d93ffdc8b44904ed0
Andrey Barfkencht Rodrigues <andrey30r@gmail.com>
2025-06-19 21:34:07 -0300

    att exe

--- exe_aula/02_condicional_10_06_25/exe32.py
+++ exe_aula/02_condicional_10_06_25/exe32.py
@@ -7,26 +7,24 @@ Acima de 5 Kg
 Morango: R$ 4,00 p/Kg
 Maçã:R$ 2,00 p/Kg
 """
+from decimal import Decimal
+
+
 kgmorango = float(input("Quantos KG de morango foi comprado: "))
 kgmaca = float(input("Quantos KG de maça foi comprado: "))
-precomorango = 0
-precomaca = 0
-if kgmorango <= 5:
-    precomorango = 5
-
+precomorango = Decimal("0")
+precomaca = Decimal("0")
+if kgmorango <= Decimal("5.00"):
+    precomorango = Decimal("5.00")
 else:
-    precomorango = 4
-
-if kgmaca <= 5:
-    precomaca = 3
+    precomorango = Decimal("4.00")
+if kgmaca <= Decimal("5.00"):
+    precomaca = "3.00"
 else:
-    precomaca = 2
+    precomaca = "2.00"
 
-
-
-
-totalmorango = kgmorango * precomorango
-totalmaca = kgmaca * precomaca
-print(totalmorango,totalmaca)
-
+totalmorango = Decimal(kgmorango) * Decimal(precomorango)
+totalmaca = Decimal(kgmaca) * Decimal(precomaca)
+print(f"\n  ------Total da compra-------\nKGs morango : {kgmorango} | total = R${round(totalmorango,2)}  ")
+print(f"\nKGs maça : {kgmaca} | total = R${round(totalmaca,2)}  ")
 
